aa6257df4a64af8b284129cd7bf228c63c50d2dc
ZairiAimenDz <zairiaimendev@gmail.com>
2021-01-29 12:47:31 +0100

    Added Authorization

--- REstate/Pages/Admin/Delete.cshtml.cs
+++ REstate/Pages/Admin/Delete.cshtml.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.EntityFrameworkCore;
@@ -10,6 +11,7 @@ using REstate.Models;
 
 namespace REstate.Pages.Admin
 {
+    [Authorize(Roles ="Admin")]
     public class DeleteModel : PageModel
     {
         private readonly REstate.Data.ApplicationDbContext _context;
